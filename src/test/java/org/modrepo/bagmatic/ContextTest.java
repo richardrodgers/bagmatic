54883e7e005cbcc4107e66386df8e742d4846c13
Richard Rodgers <richard.homerun@gmail.com>
2023-08-19 15:17:41 -0400

    Adds backing profile merge n-ary tree

--- src/test/java/org/modrepo/bagmatic/ContextTest.java
+++ src/test/java/org/modrepo/bagmatic/ContextTest.java
@@ -10,9 +10,11 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
 import java.io.IOException;
 
 import org.modrepo.bagmatic.impl.profile.BagitProfile;
+import static org.modrepo.bagmatic.Bagmatic.*;
 import org.modrepo.bagmatic.model.Context;
 import org.modrepo.bagmatic.model.Result;
 import org.modrepo.packr.Bag;
@@ -24,35 +26,63 @@ import org.modrepo.packr.Serde;
 
 public class ContextTest {
 
+    private static final String testProf = "classpath:/profiles/bagProfileFoo.json";
+    private static final String testProfBar = "classpath:/profiles/bagProfileBar.json";
+
     @Test
-    public void validatePlatformProfile() throws IllegalStateException, IOException {
-        Result<ContextBuilder> result = Bagmatic.platformedBuilder();
+    public void installProfileToEmptyBuilder() {
+        var builder = emptyBuilder();
+        Result<BagitProfile> result = builder.merge("testProf", testProf);
         assertTrue(result.success());
+        assertTrue(result.getObject().bagInfo.containsKey("Bagging-Date"));
     }
 
     @Test
-    public void readOneUserProfile() throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        //ContextBuilder builder = new ContextBuilder();
-        BagitProfile inputProfile = mapper.readValue(getClass().getResourceAsStream("/profiles/bagProfileFoo.json"),
-                                                     BagitProfile.class);
-       // builder.addProfile(getClass().getResourceAsStream("/bagProfileFoo.json"));
-       assertTrue(inputProfile.bagInfo.containsKey("Bagging-Date"));
+    public void mergeProfileToPlatformBuilder() {
+        var builder = platformBuilder();
+        Result<BagitProfile> result = builder.merge("testProf", testProf);
+        assertTrue(result.success());
+        assertTrue(result.getObject().bagInfo.containsKey("Bagging-Date"));
     }
 
     @Test
-    public void validateOneUserProfile() throws IllegalStateException, IOException {
-        ContextBuilder builder = Bagmatic.emptyBuilder();
-        Result<ContextBuilder> result = builder.addProfile(getClass().getResourceAsStream("/profiles/bagProfileFoo.json"));
-        /* 
-        for (String error : result.getErrors()) {
-            System.out.println(error);
-            System.out.flush();
-        } 
-        */
-        assertTrue(result.success());
+    public void parentOfEmptyBuilderEmpty() {
+        assert(emptyBuilder().parent().isEmpty());
     }
-    
+
+    @Test
+    public void parentOfPlatformBuilderEmpty() {
+        assert(platformBuilder().parent().isEmpty());
+    }
+
+    @Test
+    public void createSiblingOnPlatform() {
+        var builder = platformBuilder();
+        builder.merge("testProf", testProf);
+        builder.parent();
+        var result = builder.merge("testProfBar", testProfBar);
+        assertTrue(result.success());
+        assertTrue(result.getObject().bagInfo.containsKey("Contact-Email"));
+    }
+
+    @Test
+    public void findSiblingOnPlatform() {
+        var builder = platformBuilder();
+        var result1 = builder.merge("testProf", testProf);
+        assertTrue(result1.success());
+        assertTrue(result1.getObject().bagInfo.containsKey("Bagging-Date"));
+        var opt1 = builder.parent();
+        assertTrue(opt1.get().bagitProfileInfo.version.equals("0.1"));
+        var result2 = builder.merge("testProfBar", testProfBar);
+        assertTrue(result2.success());
+        assertTrue(result2.getObject().bagInfo.containsKey("Contact-Email"));
+        // now find sibling
+        var sibOpt = builder.sibling("testProf");
+        assertTrue(sibOpt.isPresent());
+        assertTrue(sibOpt.get().bagInfo.containsKey("Bagging-Date"));
+    }
+
+    /* 
     @Test
     public void bagConformsToContext() throws IOException {
         ContextBuilder builder = Bagmatic.emptyBuilder();
@@ -65,4 +95,5 @@ public class ContextTest {
             assertTrue(context.conforms(bag2).success());
         }
     }
+    */
 }
